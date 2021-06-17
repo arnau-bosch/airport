3844f9277156323cd82f1f9373fd33125abb5c04
arnau bosch <arnxbosch@gmail.com>
2021-06-17 20:40:00 +0200

    Add strapi

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4b39c597338ea5ef0b27a4acc909e0ff'),
+    },
+  },
+});
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'aa8e8055-a81d-430e-a5f5-86255113a50b'
+};
